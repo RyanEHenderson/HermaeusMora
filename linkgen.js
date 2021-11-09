b315251d07049438624a67eacf635d192930f642
Ryan Henderson <ryan.ev.henderson@gmail.com>
2021-11-09 15:48:39 -0600

    Add guilds intent, add basic validation logic

--- linkgen.js
+++ linkgen.js
@@ -5,7 +5,7 @@ const https = require('https');
 const {Client, Intents} = require('discord.js');
 const milieu = require('milieu');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = milieu('nexus', {
     bot: {
         token: ''
@@ -17,6 +17,16 @@ const token = config.bot.token;
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity('Nexus Mods');
+});
+
+client.on('messageCreate', message => {
+    if (!message.content.toLowerCase().startsWith('!getlink')) {
+        return;
+    }
+    if (!(message.attachments.size === 1 && message.attachments.first().name.toLowerCase().endsWith('.meta'))) {
+        message.reply('You must attach exactly 1 .meta file');
+        return;
+    }
 });
 
 client.login(token);
