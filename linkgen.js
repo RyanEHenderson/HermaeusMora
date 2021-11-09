5be6641ba7f1057c774dde9a1901b5f48dff9fd5
Ryan Henderson <ryan.ev.henderson@gmail.com>
2021-11-09 15:08:53 -0600

    Add basic connectivity to Discord

--- linkgen.js
+++ linkgen.js
@@ -1,5 +1,22 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+'use strict';
 
 const fs = require('fs');
 const https = require('https');
+const {Client, Intents} = require('discord.js');
+const milieu = require('milieu');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES] });
+const config = milieu('nexus', {
+    bot: {
+        token: ''
+    }
+});
+
+const token = config.bot.token;
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('Nexus Mods');
+});
+
+client.login(token);
