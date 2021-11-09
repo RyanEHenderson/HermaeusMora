4faa165b3dd5fab54fd65847ff904cd4fc3bdba4
Ryan Henderson <ryan.ev.henderson@gmail.com>
2021-11-09 16:08:32 -0600

    Implement meta parsing functionality

--- linkgen.js
+++ linkgen.js
@@ -14,12 +14,31 @@ const config = milieu('nexus', {
 
 const token = config.bot.token;
 
+function extractLink(content) {
+    let lines = content.split(/\r?\n/);
+    let gameId = '';
+    let fileId = '';
+    lines.forEach((line) => {
+        if (line.startsWith('fileID')) {
+            fileId = line.substring(line.indexOf('=')+1, line.length);
+        }
+        if (line.startsWith('url')) {
+            let temp = line.substring(line.indexOf('.com/')+5, line.length);
+            gameId = temp.substring(0, temp.indexOf('/'));
+        }
+    });
+    if (gameId === '' || fileId === '') {
+        return null;
+    }
+    return 'https://www.nexusmods.com/Core/Libs/Common/Widgets/DownloadPopUp?id=' + fileId + '&game_id=' + gameId;
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity('Nexus Mods');
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async (message) => {
     if (!message.content.toLowerCase().startsWith('!getlink')) {
         return;
     }
@@ -27,6 +46,30 @@ client.on('messageCreate', message => {
         message.reply('You must attach exactly 1 .meta file');
         return;
     }
+
+    let url = message.attachments.first().url;
+    https.get(url, (response) => {
+        response.on('error', err => {
+            message.reply('There was an error checking this file, contact Robotic');
+            console.log(err);
+            return;
+        });
+
+        let content = '';
+        response.on('data', (chunk) => {
+            content += chunk;
+        });
+
+        response.on('end', () => {
+            let link = extractLink(content);
+            if (link === null) {
+                message.reply('There was an error checking this file, contact Robotic');
+                console.log(content);
+                return;
+            }
+            message.reply('Direct download link: ' + link);
+        });
+    })
 });
 
 client.login(token);
