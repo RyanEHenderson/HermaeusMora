52e7bc65fb04167e59f1992195c911e817c3569b
Ryan Henderson <ryanehenderson@protonmail.com>
2021-11-15 00:17:35 -0600

    Add function to get files in json

--- commands/nexus.js
+++ commands/nexus.js
@@ -1,9 +1,17 @@
+const https = require('https');
 const {
     SlashCommandBuilder
 } = require('@discordjs/builders');
 const {
     nexusToken
 } = require('../config.json');
+
+const options = {
+    headers: {
+        accept: 'application/json',
+        apikey: nexusToken
+    }
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -27,10 +35,10 @@ module.exports = {
 function getLink(interaction) {
     let link = interaction.options.getString('link');
     let version = interaction.options.getString('version');
-    console.log(link);
-    console.log(version);
-    getGameName(link);
-    getModId(link);
+    let gameName = getGameName(link);
+    let modId = getModId(link);
+    getModFiles(gameName, modId);
+    console.log(modId);
 }
 
 function getGameName(link) {
@@ -38,5 +46,29 @@ function getGameName(link) {
 }
 
 function getModId(link) {
-    return link.split('/')[5];
+    let modId = link.split('/')[5];
+    if (modId.includes('?')) {
+        return modId.split('?')[0].substring(0, modId.split('?')[0].length);
+    } else {
+        return modId;
+    }
 }
+
+function getModFiles(gameName, modId) {
+    console.log(`https://api.nexusmods.com/v1/games/${gameName}/mods/${modId}/files.json`);
+    https.get(`https://api.nexusmods.com/v1/games/${gameName}/mods/${modId}/files.json`, options, (res) => {
+        res.on('error', (err) => {
+            console.log(err);
+        });
+
+        let content = '';
+        res.on('data', (chunk) => {
+            content += chunk;
+        });
+        
+        res.on('end', () => {
+            let files = JSON.parse(content);
+            return files;
+        });
+    });
+};
