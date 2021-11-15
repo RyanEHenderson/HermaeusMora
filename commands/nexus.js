b551aeffcf1890ae53e801596b9a9e9c0158fdb1
Ryan Henderson <ryanehenderson@protonmail.com>
2021-11-14 23:53:06 -0600

    Fix formatting

--- commands/nexus.js
+++ commands/nexus.js
@@ -7,16 +7,16 @@ const {
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('nexus')
-    .setDescription('Retrieves a download link from Nexus Mods')
-    .addStringOption(option =>
-        option.setName('link')
-        .setDescription('The link to the mod')
-        .setRequired(true))
-    .addStringOption(option =>
-        option.setName('version')
-        .setDescription('The version of the mod')
-        .setRequired(true)),
+        .setName('nexus')
+        .setDescription('Retrieves a download link from Nexus Mods')
+        .addStringOption(option =>
+            option.setName('link')
+            .setDescription('The link to the mod')
+            .setRequired(true))
+        .addStringOption(option =>
+            option.setName('version')
+            .setDescription('The version of the mod')
+            .setRequired(true)),
     async execute(interaction) {
         await interaction.reply('This command is not yet implemented');
     }
