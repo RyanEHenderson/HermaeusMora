c56456eaa719ada3269b77d2c9ebd7546056eb80
Ryan Henderson <ryanehenderson@protonmail.com>
2021-11-26 16:30:14 -0600

    Implement global command removal

---
+++ deploy/remove-commands-global.js
@@ -0,0 +1,28 @@
+const {
+    discordToken
+} = require('../config.json');
+
+const {
+    Client,
+    Intents
+} = require('discord.js');
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS]
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+    client.application.commands.fetch().then(commands => {
+        commands.forEach(function (command) {
+            client.application.commands.delete(command.id).then(function () {
+                console.log(`Deleted command ${command.id} (${command.name}) globally`);
+            }).catch(function (err) {
+                console.log(err);
+            });
+        });
+        client.destroy();
+    });
+});
+
+client.login(discordToken);
