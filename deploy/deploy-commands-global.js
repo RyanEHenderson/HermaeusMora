22a3cd0ca638b2987652c9b9c72adbee09cb137e
Ryan Henderson <ryanehenderson@protonmail.com>
2021-11-26 14:41:22 -0600

    Update commands folder path

--- deploy/deploy-commands-global.js
+++ deploy/deploy-commands-global.js
@@ -11,10 +11,10 @@ const {
 } = require('../config.json');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('../commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+    const command = require(`../commands/${file}`);
     commands.push(command.data.toJSON());
 }
 
